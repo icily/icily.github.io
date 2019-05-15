a28fdd169e2398bda18652f21270baa40efd027b
Chia-Yu Chen <starwithu@gmail.com>
2019-05-16 02:44:27 +0800

    Fix modal box scroll & overflow problem in Projects

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $(".other-project").on("click", function () {
   $(".modal-mask").css("display", "none");
   $(".modal-popup").css("display", "none");
-	$($(this).parents().children()[1]).toggle();       
-	$($(this).parents().children()[2]).toggle();
+//  $($(this).parents().children()[1]).toggle();       
+//  $($(this).parents().children()[2]).toggle();
+  $($(this).next()).toggle();
+  $($(this).next().children(".modal-popup")).toggle();
   
   $(document).keyup(function(e) {
 	  if (e.keyCode == 27) {
