9a6868a7b5096041326a3a4b3ce6c15b6c4a6184
Chia-Yu Chen <starwithu@gmail.com>
2019-05-16 16:03:59 +0800

    Fix modal box click inside interaction in Projects

--- js/index.js
+++ js/index.js
@@ -7,19 +7,29 @@ $(".other-project").on("click", function () {
   $($(this).next().children(".modal-popup")).toggle();
   
   $(document).keyup(function(e) {
-	  if (e.keyCode == 27) {
-  		$(".modal-mask").css("display", "none");
-	    $(".modal-popup").css("display", "none");
+    console.log("esc");
+    if (e.keyCode == 27) {
+      $(".modal-mask").css("display", "none");
+      $(".modal-popup").css("display", "none");
   	}   // esc
-	});
+  });
+  
+  $(document).click(function(event) {
+    if (!$(event.target).closest(".modal-popup,.other-project").length) {
+      $(".modal-mask").css("display", "none");
+      $(".modal-popup").css("display", "none");
+    }
+  });
 });
 
-$(".modal-mask").on("click", function(){
-  $(".modal-mask").css("display", "none");
-  $(".modal-popup").css("display", "none");
-});
 
 $(".close").on("click", function(){
   $(".modal-mask").css("display", "none");
   $(".modal-popup").css("display", "none");
 });
+
+
+//$(".modal-mask").on("click", function(){
+//  $(".modal-mask").css("display", "none");
+//  $(".modal-popup").css("display", "none");
+//});
